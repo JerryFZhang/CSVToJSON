6f3d281d1565343af63514995d2161d91d439695
zhang96 <jerry.fengwei@gmail.com>
2016-12-01 16:03:28 -0500

    Code cleanup and documentation

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 #CSVToJSON
 
 Simple Python program that converts CSV to JSON for my MongoDB project.
+- It converts all the csv files under the directory at once.
+- It just works.
+
+#Author
+Fengwei Jerry Zhang([zhang96](http://github.com/Zhang96))
+
+#License
+MIT
--- dataconverter.py
+++ dataconverter.py
@@ -6,10 +6,8 @@ for files in glob.glob("*.csv"):
     csvfile = open(files, 'r')
     jsonfile = open(files[:-4] + '.json', 'w')
     reader = csv.reader(open(files, 'rU'))
-
     fieldnames = ()
     out = 0
-
     for row in reader:
         temp = []
         # print row
@@ -18,64 +16,44 @@ for files in glob.glob("*.csv"):
         
     counter = 0
     out = ""
+    
     for row in reader:
         if counter == 0:
             # skip
             print "0"
         else:
-            print row
+            # print row
             itemCounter = 0
             temp = "{"
+        
             for item in row:
                 a = item
-                print "---------------"
-                print a
-                #print the item name.
-                # temp += a 
-                # temp += ', '
+                # print a
                 nameCounter = 0
+                
                 for item in fieldnames:
                     if itemCounter == nameCounter:
-                        # print "---------------"
-                        print item
-                        # print the fieldname that matches the location counter.
-                        # print itemCounter
-                        # print nameCounter
-                        temp+= ' "'
-                        temp+= item
-                        temp+= '"'
+                        # print item
+                        temp += ' "'
+                        temp += item
+                        temp += '"'
                         temp += ': "'
                         temp += a 
                         temp += '",'
                 
                     nameCounter += 1
-            
+                
                 itemCounter += 1
-                # print "---------------"
+            
             temp = temp[:-1]
-        
             temp += "}"
-            print temp
+            # print temp
             out += temp
             out += ", "
+        
         counter += 1
         
-        # print "******SET******"
-        # print fieldnames
-        
-                
-
-# 
-
-
-
-
-# out = json.dumps( [ row for row in reader ] )
-# print out
-# out = out.replace("[","{")
-# print out
-
-# out = out.replace("]","}")
-# print out
-
+    out = out[:-1]
     jsonfile.write(out)
+print "End of Execution"
+    
